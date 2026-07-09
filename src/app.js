37db2455daee04b7fb9f638b4f0bf6df59fc3534
NRolon <rolonnicolas.095@gmail.com>
2026-07-09 19:25:22 -0300

    Se agrega Morgan

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 import { config } from "./configs/config.js";
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
